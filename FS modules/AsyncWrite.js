4ee2b8f889d135d56532c9818a8e6e44cc528deb
Farrux Temurov <tfarrux70@gmail.com>
2025-03-16 18:36:22 +0500

    tested writing data to the file

---
+++ FS modules/AsyncWrite.js
@@ -0,0 +1,18 @@
+let fs = require('fs');
+console.log("module loaded successfully");
+let data = "Today we are learning Node.js";
+
+// fs.writeFile() takes 3 parameters
+// 1st parameter filename with path
+// 2nd parameter data to be written or message 
+// 3rd parameter callback function which take 2 parameters
+fs.appendFile('demo1.txt', data, (err) => { // fs.writeFile will override previous data
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Data stored in a file");
+    }
+});
+console.log("1st message");
+console.log("2nd message");
