b525e4d642035d14225f4bd9a703d77785575539
Farrux Temurov <tfarrux70@gmail.com>
2025-04-04 21:52:17 +0500

    get method with query param added

--- product-rest-api/app.js
+++ product-rest-api/app.js
@@ -11,6 +11,18 @@ app.get("/findProducts", (req, res) => {
     res.json(products);  // products refer to the products array above
 });
 
+// http://localhost:3000/findProductsByIdUsingQueryParam?pid=1
+app.get("/findProductsByIdUsingQueryParam", (req, res) => {
+    let pid = req.query.pid; //receiving query param value from the url
+    let result = products.find((p) => p.pid == pid);
+    if (!result) {
+        res.json({ "msg": "Product Not Found!" });
+    } else {
+        res.json(result);
+    }
+});
+
+
 app.listen(3000, () => {
     console.log('Project is running on port number 3000');
 });
