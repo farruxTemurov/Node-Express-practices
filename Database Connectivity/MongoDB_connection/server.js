bd8bdf6aecdcc3cfe38061d6232f4177b05b908b
Farrux Temurov <tfarrux70@gmail.com>
2025-04-06 06:21:17 +0500

    checking mongodb database connection

---
+++ Database Connectivity/MongoDB_connection/server.js
@@ -0,0 +1,27 @@
+let express = require("express");   // load the express module 
+let mongoDb = require("mongodb");   // load the mongodb module
+let app = express();                // create an express application    
+
+// middleware to parse JSON data from the request body
+app.use(express.json());
+
+// database URL 
+let url = "mongodb://localhost:27017"; // URL to connect to the database, 27017 is the default port for MongoDB
+let dbName = "product_db"; // name of the database to connect to
+
+// connect the mongodb database
+let mongoClient = mongoDb.MongoClient; // create a mongoClient object, which provides methods to connect to the database
+let db; // variable to hold the database object
+// ready to connect to the database 
+mongoClient.connect(url).
+    then(client => {
+        db = client.db(dbName);// connect to the database using the client object
+        console.log("Database connected successfully");
+    }).
+    catch(error => {
+        console.log(error);
+    });
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
