49b4cdeccfde52a9bb50d2e42e32ae6494e82672
Farrux Temurov <tfarrux70@gmail.com>
2025-04-13 17:31:22 +0500

    storeProduct repository

---
+++ product_crud_operations_with_mongose/repositories/products.repository.js
@@ -0,0 +1,12 @@
+let productModel = require('../models/products.models');        // ES5 style import of productModel
+let mongoose = require('mongoose');
+// this file contains more than one function with pure database logic. 
+let storeProduct = async (product) => {
+    let newProduct = new productModel(product);
+    let result = await newProduct.save();
+    return result;
+}
+
+module.exports = {
+    storeProduct
+}
