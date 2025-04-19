402a5847f2dcbca1b2d3433b84ba5e43ad6ff193
Farrux Temurov <tfarrux70@gmail.com>
2025-04-19 13:00:00 +0500

    consumed rest api and displayed it on browser

--- rest-api handling with axios/app.js
+++ rest-api handling with axios/app.js
@@ -1,9 +1,23 @@
 let axios = require("axios");
+let express = require("express");
 
-let loadFakeData = async () => {
+let app = express();
+
+app.get("/products", async (req, res) => {
     let response = await axios.get("https://fakestoreapi.com/products");
     let products = await response.data;
-    console.log(products);
-}
+    res.json(products);
+})
 
-loadFakeData();
+
+// let loadFakeData = async () => {
+//     let response = await axios.get("https://fakestoreapi.com/products");
+//     let products = await response.data;
+//     console.log(products);
+// }
+
+// loadFakeData();
+
+app.listen("3000", () => {
+    console.log("App is running on port 3000");
+});
