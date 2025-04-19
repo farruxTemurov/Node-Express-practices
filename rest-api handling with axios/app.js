e976b84dde3cc4a529be55ec4b31a6062d47db14
Farrux Temurov <tfarrux70@gmail.com>
2025-04-19 16:33:21 +0500

    data filtering using path param

--- rest-api handling with axios/app.js
+++ rest-api handling with axios/app.js
@@ -7,8 +7,21 @@ app.get("/products", async (req, res) => {
     let response = await axios.get("https://fakestoreapi.com/products");
     let products = await response.data;
     res.json(products);
-})
+});
 
+app.get("/findProduct/:pid", async (req, res) => {
+    let response = await axios.get("https://fakestoreapi.com/products");
+    let products = await response.data;
+    let pid = req.params.pid;
+
+    let product = products.find(p => p.id == pid);
+    if (product == undefined) {
+        res.resd("Product Not Found");
+    } else {
+        res.json({ "msg": "title " + product.title + "price " + product.price });
+    }
+    res.json(products);
+});
 
 // let loadFakeData = async () => {
 //     let response = await axios.get("https://fakestoreapi.com/products");
