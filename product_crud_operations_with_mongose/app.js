de030a66f951b5d5e04a72464039a51a4ab7d9f4
Farrux Temurov <tfarrux70@gmail.com>
2025-04-29 23:22:07 +0500

    implemented .env file

--- product_crud_operations_with_mongose/app.js
+++ product_crud_operations_with_mongose/app.js
@@ -1,7 +1,11 @@
 let express = require("express");
 let dbConnect = require("./config/db");
+let dotenv = require("dotenv");
+dotenv.config();
 let app = express();
 
+let PORT = process.env.PORT || 9090;
+let userRoutes = require("./routes/users.route");
 let productRoutes = require("./routes/products.route");
 
 // middleware
@@ -9,8 +13,9 @@ app.use(express.json()); // Parse JSON request body
 
 // http://localhost:3000/api/products/*
 app.use("/api/products", productRoutes.router); // Use the product routes for the API
+app.use("/api/login", userRoutes.router);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
     dbConnect.MongoDbConnect(); // calling db file function to connect to MongoDB
 })
