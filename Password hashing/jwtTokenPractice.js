06f91f3d5306c9b158021325d78625e6f40de7e9
Farrux Temurov <tfarrux70@gmail.com>
2025-04-29 23:03:25 +0500

    jwt generation and verification

---
+++ Password hashing/jwtTokenPractice.js
@@ -0,0 +1,26 @@
+let jwttoken = require("jsonwebtoken");
+let myKey = "myKey";
+
+let payloadData = {
+    "emailid": "user@gmail.com",   // value can be dynamic 
+    "role": "user",
+}
+
+let token = jwttoken.sign(payloadData, myKey, { expiresIn: '1m' })
+
+console.log("Token: ", token);
+
+// verify the token to call any end point 
+try {
+    let verifyToken = jwttoken.verify(token, myKey);
+    console.log("Verify Token: ", verifyToken);
+    if (verifyToken.role == "admin") {
+        console.log("Admin Token");
+    } else if (verifyToken.role == "user") {
+        console.log("User Token");
+    } else {
+        console.log("Guest Token");
+    }
+} catch (error) {
+    console.log(error);
+}
