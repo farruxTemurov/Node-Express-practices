6cb08ea6452b28cead2af5dfe31dddc930d2972a
Farrux Temurov <tfarrux70@gmail.com>
2025-05-03 20:04:54 +0500

    updated routes

--- product_crud_operations_with_mongose/routes/products.route.js
+++ product_crud_operations_with_mongose/routes/products.route.js
@@ -1,16 +1,20 @@
 let express = require("express");
 let router = express.Router();  // Create a new router object
 let productController = require("../controllers/products.controller");
+let myMiddleware = require("../middlewares/auth.middleware");
 // it provides http method details like get, post, put, delete etc.
 // it provides sub path and controller method to call.
 
-router.post("/store", productController.storeProduct); // Route to store product
-router.get("/findAll", productController.findAllProducts); // Route to find all products
+// admin can add, view, update and delete products
+// customer can view all products
+// any user without token can search the product.
+
+router.post("/store", myMiddleware.authMiddleware, myMiddleware.adminOnly, productController.storeProduct); // Route to store product
+router.get("/findAll", myMiddleware.authMiddleware, productController.findAllProducts); // Route to find all products
 router.get("/search/:pid", productController.searchProductById); // Route to search product by id
 router.get("/searchByPrice/:minPrice/:maxPrice", productController.findProductByPriceRange);
-router.delete("/delete/:pid", productController.deleteProductById); // Route to delete product by id
-router.put("/update/:pid", productController.updateProductPrice); // Route to update product price by id
-
+router.delete("/delete/:pid", myMiddleware.authMiddleware, myMiddleware.adminOnly, productController.deleteProductById); // Route to delete product by id
+router.put("/update/:pid", myMiddleware.authMiddleware, myMiddleware.adminOnly, productController.updateProductPrice); // Route to update product price by id
 
 module.exports = {
     router
