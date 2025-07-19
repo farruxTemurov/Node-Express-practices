714351cbe6a1f51a5c1fbdc0cecfaaf9949a902a
Farrux Temurov <tfarrux70@gmail.com>
2025-07-19 23:06:26 +0500

    Updated AsyncRead.js

--- FS modules/AsyncRead.js
+++ FS modules/AsyncRead.js
@@ -1,9 +1,9 @@
 let fs = require("fs");
 
 //1st parameter filename with path
-// 2nd parameter callback function which take 2 parameters
-// 1st parameter error
-// 2nd parameter variable which holds data
+// 2nd parameter callback function which takes 2 parameters
+// 1st parameter - error
+// 2nd parameter - variable which holds data
 fs.readFile("demo1.txt", (err, data) => {
     if (err) {
         console.log(err);
